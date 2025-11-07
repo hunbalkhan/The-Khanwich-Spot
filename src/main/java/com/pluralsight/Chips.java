65de6e3de32458ae791f8750cccb342a4ab2518b
hunbalkhan <hunbal4@gmail.com>
2025-11-07 03:29:09 -0500

    completed drinks class and chips class, however i can see i made completely different toString's and now I will try to go ahead and clean them up to make them be more consistent and look nicer!

--- src/main/java/com/pluralsight/Chips.java
+++ src/main/java/com/pluralsight/Chips.java
@@ -3,5 +3,25 @@ package com.pluralsight;
 public class Chips extends Product{
 
     // Represents a bag of chips (type/flavor).
+    private String flavor;
+
+    public Chips(String flavor) {
+        super("Chips (" + flavor + ")", 1.50);
+        this.flavor = flavor;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    @Override
+    public double calculatePrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Chips - " + flavor + " | Price: $" + String.format("%.2f", calculatePrice());
+    }
 
 }
--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -4,4 +4,39 @@ public class Drink extends Product{
 
     // Represents a drink with a size and flavor.
 
+    private String size;
+    private String flavor;
+
+    public Drink(String flavor, String size) {
+        super(flavor + " (" + size + ")", getPriceBySize(size)); // automatically sets name and price, different from before
+        this.flavor = flavor;
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+    public String getFlavor() {
+        return flavor;
+    }
+
+    private static double getPriceBySize(String size) {
+        switch (size.toLowerCase()) {
+            case "small": return 1.50;
+            case "medium": return 2.00;
+            case "large": return 2.50;
+            default: return 0.0;
+        }
+    }
+
+    @Override
+    public double calculatePrice() {
+        // since price is fixed by size we just need to return price
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Drink - flavor= " + flavor + ", size= " + size + ", price = $" + getPrice();
+    }
 }
